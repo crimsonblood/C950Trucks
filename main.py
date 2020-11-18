58f80a9e806968d1436b934674c6aefc90cb7225
Evan Blanke <evanblanke@gmail.com>
2020-11-17 19:39:29 -0600

    asd

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
 # This is a sample Python script.
+from packageImport import get_hash_map
+from packages import total_distance
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Main:
+    print(get_hash_map().get_value(str(38)))
+
+    print('-' * 20)
+    print('WGUPS Routing Program')
+    print('-' * 20)
+    print(f'Route was completed in {total_distance():.2f} miles.\n')
+    #route total distance print
+
+    n = input("""Please select an option below or type 'exit' to exit: 
+    1. Display all packages at time
+    2. Lookup specific package
+    """)
+
+    while n != 'exit':
+        #option 1: display all packages at time
+        if n == '1':
+            try:
+                input_time = input('Enter a time (HH:MM:SS): ')
+                (hrs, mins, secs) = input_time.split(':')
+                convert_user_time = datetime.timedelta(int(hrs), int(mins), int(secs))
+
+                # Complexity ->  O(n^2)
+                for count in range(1, 40):
+                    print(get_hash_map().get_value(str(count)))
+                    try:
+                        first_time = get_hash_map().get_value(str(count))[9]
+                        second_time = get_hash_map().get_value(str(count))[10]
+                        (hrs, mins, secs) = first_time.split(':')
+                        convert_first_time = datetime.timedelta(int(hrs), int(mins), int(secs))
+                        (hrs, mins, secs) = second_time.split(':')
+                        convert_second_time = datetime.timedelta(int(hrs), int(mins), int(secs))
+                    except ValueError:
+                        pass
+
+                    # Determine which packages have left the hub
+                    if convert_first_time >= convert_user_time:
+                        get_hash_map().get_value(str(count))[10] = 'At Hub'
+                        get_hash_map().get_value(str(count))[9] = 'Leaves at ' + first_time
+
+                        # Print package's current info
+                        print(
+                            f'Package ID: {get_hash_map().get_value(str(count))[0]}, '
+                            f'Delivery status: {get_hash_map().get_value(str(count))[10]}'
+                        )
+
+                    # Determine which packages have left but have not been delivered
+                    elif convert_first_time <= convert_user_time:
+                        if convert_user_time < convert_second_time:
+                            get_hash_map().get_value(str(count))[10] = 'In transit'
+                            get_hash_map().get_value(str(count))[9] = 'Left at ' + first_time
+
+                            # Print package's current info
+                            print(
+                                f'Package ID: {get_hash_map().get_value(str(count))[0]}, '
+                                f'Delivery status: {get_hash_map().get_value(str(count))[10]}'
+                            )
+
+                        # Determine which packages have already been delivered
+                        else:
+                            get_hash_map().get_value(str(count))[10] = 'Delivered at ' + second_time
+                            get_hash_map().get_value(str(count))[9] = 'Left at ' + first_time
+
+                            # Print package's current info
+                            print(
+                                f'Package ID: {get_hash_map().get_value(str(count))[0]}, '
+                                f'Delivery status: {get_hash_map().get_value(str(count))[10]}'
+                            )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            except IndexError:
+                print(IndexError)
+                exit()
+            except ValueError:
+                print('Invalid input')
+                exit()
 
+        #option 2: display specific package
+        elif n == '2':
+            try:
+                print('Option 2 stuff')
+            except ValueError:
+                print('Invalid input')
+                exit()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #exit
+        elif n == 'exit':
+            exit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        else:
+            print('Invalid input')
+            exit()
